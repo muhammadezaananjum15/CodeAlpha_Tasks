90761f9e8d81dc936acd24caed7db299019b0f87
muhammadezaananjum289 <muhammadezaananjum289@gmail.com>
2026-01-11 15:39:19 +0500

    Uploading Task 2

---
+++ CodeAlpha_ImageGallery/app.js
@@ -0,0 +1,32 @@
+const filterBtns = document.querySelectorAll('.filter-btn');
+const items = document.querySelectorAll('.gallery-item');
+
+filterBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    filterBtns.forEach((b) => b.classList.remove('active'));
+    btn.classList.add('active');
+
+    const filter = btn.dataset.filter;
+
+    items.forEach((item) => {
+      if (filter === 'all') {
+        item.classList.remove('hide');
+      } else {
+        item.classList.toggle('hide', !item.classList.contains(filter));
+      }
+    });
+  });
+});
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modalImg');
+const closeModal = document.getElementById('closeModal');
+
+items.forEach((item) => {
+  item.addEventListener('click', () => {
+    modal.classList.add('active');
+    modalImg.src = item.querySelector('img').src;
+  });
+});
+
+closeModal.onclick = () => modal.classList.remove('active');
+modal.onclick = (e) => e.target === modal && modal.classList.remove('active');
